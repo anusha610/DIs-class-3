fabac7cacbd9bb86ce1b7c399170242590f4aa35
anush <anush@DESKTOP-RQ8J175>
2020-02-07 11:54:45 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIs_class_3
+{
+    class Class1
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DIs_class_3
+{
+      class Program
+    {
+        string color;
+        int maxSpeed;
+        static void Main(string[] args)
+        {
+            Program myObj = new Program();
+            myObj.color = "red";
+            myObj.maxSpeed = 200;
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+        }
+    }
+}
